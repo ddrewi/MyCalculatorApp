7b3f2fefe7f5bed6913ae280d7299007a767b811
ddrewi <84168601+ddrewi@users.noreply.github.com>
2022-08-25 12:55:09 -0500

    worked on Snackbar

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +18,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void snackbarClicked(View view) {
+        Snackbar.make(view, "Please Enter A Number", Snackbar.LENGTH_INDEFINITE)
+                .setAnchorView(findSum())
+                .setBackgroundTint(getResources().getColor(R.color.olive))
+                .setTextColor(getResources().getColor(R.color.black))
+                .setAction("Got It", new View.OnClickListener(){
+                    public void onClick(View view) {
+                        makeToast("asdf");
+                    }
+                })
+                .setActionTextColor(getResources().getColor(R.color.purple_200))
+                .show();
+    }
+
+    public void makeToast  (String str) {
+        Toast toast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG);
+        toast.show();
+    }
 
     public void findSum(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        double num1 = Double.parseDouble((number1ET.getText().toString()));
-        double num2 = Double.parseDouble((number2ET.getText().toString()));
-        double sum = num1 + num2;
+        try {
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double sum = num1 + num2;
 
-        numberSumTV.setText("" + sum);
+            numberSumTV.setText("" + sum);
+        }
+
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
     }
 
     public void findDifference(View view) {
