b6e1b9ae0d72a592cbf8f07e901adfa3a1757567
ddrewi <andrew.mdi130@gmail.com>
2022-08-26 06:47:21 -0500

    finalized project

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -16,26 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        EditText num1ET = findViewById(R.id.num1ET);
+        EditText num2ET = findViewById(R.id.num2ET);
+
+        // hide the keyboard when entering numbers
+        num1ET.setShowSoftInputOnFocus(false);
+        num2ET.setShowSoftInputOnFocus(false);
+
+    }
+    // snackbar formatting taken from Schoology
     public void snackbarClicked(View view) {
         Snackbar.make(view, "Please Enter A Number", Snackbar.LENGTH_INDEFINITE)
-                .setAnchorView(findSum())
-                .setBackgroundTint(getResources().getColor(R.color.olive))
-                .setTextColor(getResources().getColor(R.color.black))
+                .setBackgroundTint(getResources().getColor(R.color.blue))
+                .setTextColor(getResources().getColor(R.color.yellow))
                 .setAction("Got It", new View.OnClickListener(){
                     public void onClick(View view) {
-                        makeToast("asdf");
                     }
                 })
-                .setActionTextColor(getResources().getColor(R.color.purple_200))
+                .setActionTextColor(getResources().getColor(R.color.yellow))
                 .show();
     }
 
     public void makeToast  (String str) {
-        Toast toast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT);
         toast.show();
     }
+
+    //try catch to find errors
 
     public void findSum(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
@@ -60,11 +68,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberDifferenceTV = findViewById(R.id.resultTV);
 
-        double num1 = Double.parseDouble((number1ET.getText().toString()));
-        double num2 = Double.parseDouble((number2ET.getText().toString()));
-        double difference = num1 - num2;
+        try {
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double difference = num1 - num2;
 
-        numberDifferenceTV.setText("" + difference);
+            numberDifferenceTV.setText("" + difference);
+        }
+
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
     }
 
     public void findProduct(View view) {
@@ -72,11 +86,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberProductTV = findViewById(R.id.resultTV);
 
-        double num1 = Double.parseDouble((number1ET.getText().toString()));
-        double num2 = Double.parseDouble((number2ET.getText().toString()));
-        double product = num1 * num2;
+        try {
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double product = num1 * num2;;
 
-        numberProductTV.setText("" + product);
+            numberProductTV.setText("" + product);
+        }
+
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
     }
 
     public void findQuotient(View view) {
@@ -84,13 +104,17 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberQuotientTV = findViewById(R.id.resultTV);
 
-        double num1 = Double.parseDouble((number1ET.getText().toString()));
-        double num2 = Double.parseDouble((number2ET.getText().toString()));
-        double quotient = num1 / num2;
+        try {
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double quotient = num1 / num2;
 
-        numberQuotientTV.setText("" + quotient);
+            numberQuotientTV.setText("" + quotient);
+        }
 
-        // try catch
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
     }
 
     public void findExponent(View view) {
@@ -98,11 +122,63 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberExponentTV = findViewById(R.id.resultTV);
 
-        double num1 = Double.parseDouble((number1ET.getText().toString()));
-        double num2 = Double.parseDouble((number2ET.getText().toString()));
-        double exponent = Math.pow(num1, num2);
+        try{
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double exponent = Math.pow(num1, num2);
 
-        numberExponentTV.setText("" + exponent);
+            numberExponentTV.setText("" + exponent);
+        }
+
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
+    }
+
+    public void findLog(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberLogTV = findViewById(R.id.resultTV);
+
+        try {
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            // use change of base log formula
+            double log = Math.log(num1)/Math.log(num2);
+
+            numberLogTV.setText("" + log);
+        }
+
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
+    }
+
+    public void findMod(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberModTV = findViewById(R.id.resultTV);
+
+        try {
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            // use change of base log formula
+            double mod = num1 % num2;
+
+            numberModTV.setText("" + mod);
+        }
+
+        catch (Exception e) {
+            snackbarClicked(view);
+        }
+    }
+
+    public void clear(View view){
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+
+        number1ET.getText().clear();
+        number2ET.getText().clear();
     }
 
 
